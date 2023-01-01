bcc378dc49efc84e6056d478e9681e0160256f86
Uzma FKhan <uzmafkhan24@gmail.com>
2023-01-01 17:13:59 +0530

    q2

--- Microsoft/Combination_Sum_III.cpp
+++ Microsoft/Combination_Sum_III.cpp
@@ -1,8 +1,39 @@
-#include <iostream>
-using namespace std;
-
-int main(){
+class Solution {
+    vector<vector<int>> ansArr;
     
-    return 0;
-}
-// g++ --std=c++17 -Wall -Wextra Combination_Sum_III.cpp && ./a.out
+public:
+    
+    int sum(vector<int>& combination){
+        int ans = 0;
+        for(auto x: combination){
+            ans += x;
+        }
+        return ans;
+    }
+    
+    void combinationSumHelper(int k, int n, vector<int>& combination, int num){
+        
+        if(k == 0){
+            if(sum(combination) == n ){
+                ansArr.push_back(combination);
+            }
+            return;
+        }
+        
+        if(num > 9) return;
+        
+        combination.push_back(num);
+        combinationSumHelper(k - 1, n, combination, num + 1);
+        combination.pop_back();
+        combinationSumHelper(k, n, combination, num + 1);    
+        
+    }
+    
+    vector<vector<int>> combinationSum3(int k, int n) {
+        
+        vector<int> combination;
+        combinationSumHelper(k, n, combination, 1);
+        return ansArr;
+        
+    }
+};
