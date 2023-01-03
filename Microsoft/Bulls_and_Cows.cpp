375d6a5bcab43bb98de36ddd2315f9c8e8973a45
Uzma FKhan <uzmafkhan24@gmail.com>
2023-01-03 12:14:51 +0530

    q3

--- Microsoft/Bulls_and_Cows.cpp
+++ Microsoft/Bulls_and_Cows.cpp
@@ -1,8 +1,34 @@
-#include <iostream>
-using namespace std;
+class Solution {
+public:
+    string getHint(string secret, string guess) {
+        
+        int b=0,c=0,n=secret.length();
 
-int main(){
-    
-    return 0;
-}
-// g++ --std=c++17 -Wall -Wextra Bulls_and_Cows.cpp && ./a.out
+        map <char,int>m1;
+        map <char,int>m2;
+        
+        for(int i=0;i<n;i++){
+            if(guess[i]==secret[i]){
+                b++;
+            }else{
+                m1[guess[i]]++;
+                m2[secret[i]]++;
+            }
+        }
+
+        for(auto i:m1){
+            char c=i.first;
+            if(m2.find(c)!=m2.end()){
+                c+=min(m1[c],m2[c]);
+            }
+        }
+        
+        string s="";
+        s+=('0'+b);
+        s+='A';
+        s+=('0'+c);
+        s+='B';
+
+        return s;
+    }
+};
